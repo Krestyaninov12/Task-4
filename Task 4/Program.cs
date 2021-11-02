41f5c39896b12877a032ac00723b3902d163f34b
Krestyaninov12 <kreda@pnpe.ru>
2021-11-02 14:30:39 +0300

    Добавьте файлы проекта.

---
+++ Task 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, sum, i;
+            Console.WriteLine("Введите значение N >0");
+            N = Convert.ToInt16(Console.ReadLine());
+            i = 1;
+            sum = 0;
+
+            for (; i <= ((2 * N) - 1);)
+
+            {
+                sum += i;
+                Console.WriteLine("Текущее значение суммы {0} ", sum);
+                i += 2;
+            }
+                        
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
